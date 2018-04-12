358b1548ae51779f7f944eda7897c72ab117bcb9
lmedeiro <medeirosluizf@gmail.com>
2018-04-13 00:56:19 +0300

    feature: implemented ability to calculate abundance
    - implemented get_proper_divisors
    - .. sum_items_in_array()
    - .. sum_of_divisors()
    - .. get_abundance()

---
+++ prerequisite_LFM/src/abundant.cc
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : abundant.cc
+// Author      : Luiz Fernando Medeiros
+// Version     :
+// Copyright   : Free to use, without any guarantees ! 
+// Description : Generating N abundant numbers in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
